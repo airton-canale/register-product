f9c5aa4e1f2ff281309c29990e2315eff56c761a
Leonardo <lelazzati7@gmail.com>
2023-05-16 15:13:35 -0300

    fix: body parser

--- backend/src/infra/adapters/express-router-adapter.js
+++ backend/src/infra/adapters/express-router-adapter.js
@@ -1,13 +1,14 @@
 const adaptRoute = (controller) => {
-    return async (req, res) => {
-          const httpRequest = {
-            query: req.query,
-            body: req.body,
-            params: req.params
-          }
-          const httpResponse = await controller.handle(httpRequest)
-          return res.status(httpResponse.status).json(httpResponse?.data).send()
-        }
+  return async (req, res) => {
+    console.log('eex', req.body)
+    const httpRequest = {
+      query: req.query,
+      body: req.body,
+      params: req.params
     }
-        
-    export { adaptRoute }
+    const httpResponse = await controller(httpRequest)
+    return res.status(httpResponse.status).json(httpResponse?.data).send()
+  }
+}
+
+export { adaptRoute }
